5a06e98b24d7f281039c32ea7f974efdd8df9e37
Thimira Dilanga <79562923+dilanga2002@users.noreply.github.com>
2025-07-01 01:39:34 +0530

    Initial commit

---
+++ storage/framework/views/253fe2157fcd2708babc285f961aa7f7.php
@@ -0,0 +1,54 @@
+<?php if (isset($component)) { $__componentOriginalaa758e6a82983efcbf593f765e026bd9 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalaa758e6a82983efcbf593f765e026bd9 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => $__env->getContainer()->make(Illuminate\View\Factory::class)->make('mail::message'),'data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('mail::message'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+# New Contact Form Submission
+
+**Name:** <?php echo new \Illuminate\Support\EncodedHtmlString($name); ?>  
+**Email:** <?php echo new \Illuminate\Support\EncodedHtmlString($email); ?>  
+**Subject:** <?php echo new \Illuminate\Support\EncodedHtmlString($subject); ?>  
+**Message:**  
+<?php echo new \Illuminate\Support\EncodedHtmlString($messageContent); ?>
+
+
+<?php if (isset($component)) { $__componentOriginal15a5e11357468b3880ae1300c3be6c4f = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal15a5e11357468b3880ae1300c3be6c4f = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => $__env->getContainer()->make(Illuminate\View\Factory::class)->make('mail::button'),'data' => ['url' => 'mailto:' . $email]] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('mail::button'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['url' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute('mailto:' . $email)]); ?>
+Reply to Sender
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal15a5e11357468b3880ae1300c3be6c4f)): ?>
+<?php $attributes = $__attributesOriginal15a5e11357468b3880ae1300c3be6c4f; ?>
+<?php unset($__attributesOriginal15a5e11357468b3880ae1300c3be6c4f); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal15a5e11357468b3880ae1300c3be6c4f)): ?>
+<?php $component = $__componentOriginal15a5e11357468b3880ae1300c3be6c4f; ?>
+<?php unset($__componentOriginal15a5e11357468b3880ae1300c3be6c4f); ?>
+<?php endif; ?>
+
+Thanks,<br>
+<?php echo new \Illuminate\Support\EncodedHtmlString(config('app.name')); ?>
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalaa758e6a82983efcbf593f765e026bd9)): ?>
+<?php $attributes = $__attributesOriginalaa758e6a82983efcbf593f765e026bd9; ?>
+<?php unset($__attributesOriginalaa758e6a82983efcbf593f765e026bd9); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalaa758e6a82983efcbf593f765e026bd9)): ?>
+<?php $component = $__componentOriginalaa758e6a82983efcbf593f765e026bd9; ?>
+<?php unset($__componentOriginalaa758e6a82983efcbf593f765e026bd9); ?>
+<?php endif; ?><?php /**PATH C:\Users\Dell\Desktop\sritravel\sritravel\resources\views/emails/contact.blade.php ENDPATH**/ ?>
